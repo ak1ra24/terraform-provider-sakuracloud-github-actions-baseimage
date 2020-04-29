f25464b7fe419671aded7cc425c29e0cbad1227a
ak1ra24 <ak1ra24net@gmail.com>
2020-04-29 10:33:11 +0900

    del terraform init

--- install.sh
+++ install.sh
@@ -19,28 +19,4 @@ credentials "${TF_ACTION_TFE_HOSTNAME:-app.terraform.io}" {
 EOF
 fi
 
-set +e
-export TF_APPEND_USER_AGENT="terraform-github-actions/1.0"
-OUTPUT=$(sh -c "terraform init -no-color -input=false $*" 2>&1)
-SUCCESS=$?
-echo "$OUTPUT"
-set -e
-
-if [ $SUCCESS -eq 0 ]; then
-    exit 0
-fi
-
-if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
-    exit $SUCCESS
-fi
-
-COMMENT="#### \`terraform init\` Failed
-\`\`\`
-$OUTPUT
-\`\`\`
-*Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
-PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
-
-exit $SUCCESS
+exit 0
